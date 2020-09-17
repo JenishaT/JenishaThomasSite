543fb0a2d9b2515119675efb4b198071b44f49ee
unknown <jenishathomas00@gmail.com>
2020-09-17 15:55:19 -0400

    Created theme file

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "rgb(68, 164, 241)"
+    },
+    secondary: {
+      main: "rgb(22,67,95)"
+    }
+  }
+});
+
+export default theme;
